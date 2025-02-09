9aa3232347b196609b25f7e96521824d2efa61b2
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2025-02-09 14:02:16 +0100

    Mobile support

--- Frontend/app/root.tsx
+++ Frontend/app/root.tsx
@@ -28,6 +28,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-title" content="MovieList" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="apple-touch-icon" href="/public/icon-192x192.png" />
         <Meta />
         <Links />
       </head>
