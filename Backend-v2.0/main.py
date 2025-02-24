bd1ab19c757c0dc37381126e2afbbc29590f7ce5
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2025-02-24 22:40:41 +0100

    Backend-v2.0 -> Added extra config

--- Backend-v2.0/main.py
+++ Backend-v2.0/main.py
@@ -15,6 +15,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://workout:workout@192.168.178.228:3502/workout'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'ya172914d82aa8cc99c5e5ae937acfdfbd6144a0291bf978fc63ce0656aaa89787dc0bafcb5cae384990acc2434f1311840524a71cb7dff644b4bce60bf7f19a32869b62d7b63eba48442b0757a77524b931996181c1dd910bbfdcb3a5bdb6acddfc29be08522eee645a7cace12937133f4b1052a7a6ae198c03563fe1dc169316a1bca94787a17ee8dc8dadac3fd3bc4cc28408a00969cf71884828dd8ccb1fd4a3ec0a9bc869e88ab19f3037e6079011dc0d01edb445cf2ee4b3fa1417d03a42dc071b8c9ec710cb6a7dd63c1241de94b0fbb9267e22d080cdc9cb48d95e52929ec8c955d10503aa4b5a5aa09325dcbf2af5f885d8ee7b38c0a0cc990905efd'  
+app.config['PREFERRED_URL_SCHEME'] = 'https'
+app.config['FORWARDED_ALLOW_IPS'] = '*'
+app.config['RESTX_MASK_SWAGGER'] = False
+app.config['RESTX_VALIDATE'] = True
+app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 4}
+app.config['RESTX_ERROR_404_HELP'] = False
+app.config['RESTX_TRAILING_SLASH'] = False
 db.init_app(app)
 
 jwt = JWTManager(app)
