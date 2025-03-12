4cbb72352ac8eebd7d7d0e43c626782a2f30a320
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2025-03-12 13:18:00 +0100

    Backend -> Logic update

---
+++ Backend-v2.0/database.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Users (
+    id INTEGER PRIMARY KEY,
+    username VARCHAR(80) UNIQUE NOT NULL,
+    email VARCHAR(120) UNIQUE NOT NULL,
+    password VARCHAR(120) NOT NULL
+);
+
+CREATE TABLE Categories (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(200),
+    type VARCHAR(80)
+);
+
+CREATE TABLE Exercises (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(200),
+    routine_id INTEGER NOT NULL,
+    FOREIGN KEY (routine_id) REFERENCES Routines(id)
+);
+
+CREATE TABLE ExerciseCategories (
+    id INTEGER PRIMARY KEY,
+    exercise_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (exercise_id) REFERENCES Exercises(id),
+    FOREIGN KEY (category_id) REFERENCES Categories(id)
+);
+
+CREATE TABLE Routines (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(200)
+);
+
+CREATE TABLE RoutineCategories (
+    id INTEGER PRIMARY KEY,
+    routine_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    FOREIGN KEY (routine_id) REFERENCES Routines(id),
+    FOREIGN KEY (category_id) REFERENCES Categories(id)
+);
